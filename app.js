a518f8676dfb847d77353890e10553f9de643ce6
apple <omotayoadegoke19@gmail.com>
2023-04-30 10:49:25 +0100

    updated env

--- app.js
+++ app.js
@@ -14,15 +14,16 @@ app.use(cors());
 app.use("/comments", commentsRoutes);
 app.use("/users", userRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8j43x.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     app.listen(PORT, () => console.log(`app running on port ${PORT}`));
   })
